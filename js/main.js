fe0057758559dabe6180f5e35217274e822aed8a
Wilson Parada <cxrkeybwp2004@gmail.com>
2021-09-02 09:12:05 -0500

    Hice el desafio 1 y creé un bg en el html

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
 let nombre = prompt('Ingrese su nombre');
 let apellido = prompt('Ingrese su apellido');
+let edad = prompt('¿Cuál es tú edad?');
+let animal = prompt('¿Cuál es tú animal favorito?')
 
-console.log(nombre, apellido); 
+let fechaDeNacimiento = parseInt(edad);
+
+
+console.log('Bienvenido ',nombre, apellido);
+console.log('Tú fecha de nacimiento fue en ' + (2021 - fechaDeNacimiento) + ' ' + 'y tienes ' + edad + ' años');
+console.log('Tú animal favorito es el/la ' + animal)
+console.log('Dentro de 10 años tendrás ' + (fechaDeNacimiento + 10) + ' años')

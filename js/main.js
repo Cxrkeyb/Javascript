8cb52626705411b1270ca6f989e9ba20b5cbdbab
Wilson Parada <cxrkeybwp2004@gmail.com>
2021-09-02 19:55:17 -0500

    Junté el desafio 1 y 2 para las entregas

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+// Desafio 1
+let nombre = prompt('Ingrese su nombre');
+let apellido = prompt('Ingrese su apellido');
+let edad = prompt('¿Cuál es tú edad?');
+let animal = prompt('¿Cuál es tú animal favorito?')
+
+let fechaDeNacimiento = parseInt(edad);
+
+
+console.log('Bienvenido ',nombre, apellido);
+console.log('Tú fecha de nacimiento fue en ' + (2021 - fechaDeNacimiento) + ' ' + 'y tienes ' + edad + ' años');
+console.log('Tú animal favorito es el/la ' + animal)
+console.log('Dentro de 10 años tendrás ' + (fechaDeNacimiento + 10) + ' años')
+
+// Desafio 2
 let edad = prompt('¿Cuantos años tienes?');
 let mayoria = parseInt(edad);
 let fruta = prompt ('¿Qué fruta prefieres manzana o pera?');
